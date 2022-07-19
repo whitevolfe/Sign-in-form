98a45b2a2054f92def5bd50bf7dee32c09cea0ba
whitevolfe <azeemshanr@gmail.com>
2022-07-19 20:29:25 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import SignIn from './SignIn';
+
+function App() {
+  return (
+    <div className="App">
+      <SignIn />
+     
+    </div>
+  );
+}
+
+export default App;
